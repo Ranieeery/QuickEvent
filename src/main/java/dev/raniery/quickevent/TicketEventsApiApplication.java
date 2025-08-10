1a44229535d127b0ac595d4a26dd79e62f717c5f
Ranieeery <raniery2003@hotmail.com>
2025-08-09 23:28:39 -0300

    feat: initialize project structure with Maven, add PostgreSQL configuration, and create application entry point

---
+++ src/main/java/dev/raniery/quickevent/TicketEventsApiApplication.java
@@ -0,0 +1,13 @@
+package dev.raniery.quickevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketEventsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketEventsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/raniery/quickevent/TicketEventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raniery.quickevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketEventsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
